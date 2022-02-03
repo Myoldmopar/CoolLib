324f1c6d6c551672eec3240297b814d38df88563
Edwin Lee <leeed2001@gmail.com>
2022-02-03 17:39:04 -0600

    Add dllexports to library

--- cool_lib.h
+++ cool_lib.h
@@ -1,6 +1,17 @@
 #ifndef MY_COOL_LIB_H
+
+#if (_WIN32 || _MSC_VER)
+#if defined(CoolLib_EXPORTS)
+#define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API __declspec(dllimport)
+#endif
+#else
+#define EXPORT_API
+#endif
+
 namespace MyCoolLib {
-    int add(int x, int y);
+    EXPORT_API int add(int x, int y);
 }
 #define MY_COOL_LIB_H
 #endif
